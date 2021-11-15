e11d4e7e8c7a3a6acda94b50b1ee97bfbbf906ef
jaredhall18 <jaredhall18@gmail.com>
2021-11-15 14:19:29 -0700

    created forms for login and signup pages, There is a placeholder for api calls once the backend is complete.

--- anywhere-fitness/src/components/SignUp.js
+++ anywhere-fitness/src/components/SignUp.js
@@ -1,9 +1,77 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { useHistory } from "react-router-dom";
+import axios from "axios";
+
+const initialFormErrors = {
+    username: "",
+    password: "",
+  };
 
 export default function SignUp() {
+    const [form, setForm] = useState({
+        credentials: {
+          username: "",
+          password: "",
+        },
+      });
+    
+      //Disabled
+      const [disabled, setDisabled] = useState(true);
+      //Errors
+      const [formErrors, setFormErrors] = useState(initialFormErrors);
+    
+      const { push } = useHistory();
+    
+      const handleChange = (e) => {
+        const name = e.target.name;
+        const value = e.target.value;
+
+        setForm({
+          credentials: { ...form.credentials, [name]: value },
+        });
+      };
+
+    const userSignUp = (e) => {
+        e.preventDefault();
+        axios
+          .post(
+            "https://testurl.com",
+            form.credentials
+          )
+          .then((res) => {
+            console.log(res.data);
+            localStorage.setItem("token", res.data.token);
+            push("/login");
+          })
+          .catch((err) => {
+            console.log(err.response);
+          });
+      };
+
     return (
-        <div>
-            <p>signup</p>
+       <div>
+            <h2>Please Create An Account</h2>
+            <form onSubmit={userSignUp}>
+            <label>
+                Username:
+                <input
+                    type="text"
+                    name="username"
+                    value={form.username}
+                    onChange={handleChange}
+                />
+            </label>
+            <label>
+                Password:
+                <input
+                    type="text"
+                    name="password"
+                    value={form.password}
+                    onChange={handleChange}
+                />
+            </label>
+                <button>Create</button>
+            </form>
         </div>
     )
 }
--- anywhere-fitness/src/components/login.js
+++ anywhere-fitness/src/components/login.js
@@ -1,9 +1,76 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { useHistory } from "react-router-dom";
 
-export default function login() {
+const initialFormErrors = {
+    username: "",
+    password: "",
+  };
+
+export default function Login() {
+    const [state, setState] = useState({
+        credentials: {
+          username: "",
+          password: "",
+        },
+      });
+
+    // Disabled Button
+    const [disabled, setDisabled] = useState(true);
+    // Form Errors
+     const [formErrors, setFormErrors] = useState(initialFormErrors);
+
+    // const { push } = useHistory();
+      
+    const login = (e) => {
+        e.preventDefault();
+    //     axiosWithAuth()
+    //       .post("auth/login", state.credentials)
+    //       .then((res) => {
+    //         setFormErrors(initialFormErrors);
+    //         localStorage.setItem("token", res.data.token);
+    //         push("/upcomingevents");
+    //         props.setLoggedIn(true);
+    //       })
+    //       .catch((err) => {
+    //         setFormErrors({
+    //           ...formErrors,
+    //           navValid: "this account does not exist",
+    //         });
+    //       });
+    };
+
+    const handleChange = (e) => {
+        const name = e.target.name;
+        const value = e.target.value;
+    
+        setState({
+          credentials: { ...state.credentials, [name]: value },
+        });
+    };
+
     return (
         <div>
-            <p>Login</p>
+            <form onSubmit={login}>
+                <label>
+                    Username:
+                    <input
+                        type="text"
+                        name="username"
+                        value={state.username}
+                        onChange={handleChange}
+                    />
+                </label>
+                <label>
+                    Password:
+                    <input
+                        type="text"
+                        name="password"
+                        value={state.password}
+                        onChange={handleChange}
+                    />
+                </label>
+                <button>Login</button>
+            </form>
         </div>
-    )
+    );
 }
