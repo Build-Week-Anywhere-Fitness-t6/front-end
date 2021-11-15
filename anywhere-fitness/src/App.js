741f7f7f5d4a1d172bda5b41318ea7a996f76194
jaredhall18 <jaredhall18@gmail.com>
2021-11-15 10:28:30 -0700

    Created app and some initial file structure.

---
+++ anywhere-fitness/src/App.js
@@ -0,0 +1,13 @@
+import { Route, Switch, Redirect } from "react-router-dom";
+import React, { useState } from "react";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Anywhere Fitness</h1>
+    </div>
+  );
+}
+
+export default App;
